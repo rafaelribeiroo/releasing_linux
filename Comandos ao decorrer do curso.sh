0780478fd7fa0048af2758ad15b9a286b9a28eb2
Rafael <pereiraribeirorafael@gmail.com>
2019-08-12 16:23:45 -0300

    utils

--- Comandos ao decorrer do curso.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Ao trabalhar com o bash, ele armazena os últimos 2.000 comandos informados.
-# Há formas de expandir esse limite, porém não interessa no momento, para
-# buscar esses registros usamos o:
-history
-# E para executar um, basta informar !<posição_comando>

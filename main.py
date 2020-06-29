6f23aa62aedb60ec0e2d374a47647e4984cf5064
Rafael Ribeiro <pereiraribeirorafael@gmail.com>
2020-06-29 01:40:58 -0300

    mudanças gerais, adições e remoções

---
+++ main.py
@@ -0,0 +1,8 @@
+# 98. Faça um programa que tenha uma função chamada contador(), que receba três
+# parâmetros: início, fim e passo e realize a contagem. Seu programa tem que
+# realizar três contagens através da função criada:
+# > De 1 até 10, de 1 em 1
+# > De 10 até 0, de 2 em 2
+# > Uma contagem personalizada.
+
+print('Olá')
